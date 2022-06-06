1d79669ad06b41e9305fbbf5523bafca44f2060d
Tobiias1996 <Charlemagne80@gmx.de>
2022-06-06 15:58:09 +0200

    edit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
 # work in progress
-
+a=1
 DeinName=input("Wie heißt du?\n")
 BistDuMutig=input("Bist du mutig? Tippe 'Ja' oder 'Nein'.\n")
 
@@ -27,6 +27,8 @@ if (Start=="2" or Augengeschlossen =="1"):
                 print(f"Das war leider keine gute Idee von dir {DeinName}. Die Distanz von Fenster und Boden war zu hoch... Ende. ")
             elif(Fensterklettern==6):
                 print(f"Du landest ächzend auf deinen Füßen. Dir wird sofort bewusst, wie viel Glück du gerade gehabt hast. Doch das ist egal. Du willst nur weg von diesem unbekannten Haus. Du atmest einmal tief durch und läufst davon. Ende.")
+            else:
+                print("fehlerhafte Eingabe")
         if (Fenster=="2"):
             NichtausdemFensterspringen=input(f"Du beschließt, dass dir ein Sprung ins Ungewisse zu riskant ist. Du entscheidest dich für die Türe. Drücke 1 um fortzufahren  ")
     if (Raum=="2" or NichtausdemFensterspringen=="1"):
@@ -37,14 +39,31 @@ if (Start=="2" or Augengeschlossen =="1"):
             Bibliothek=input("Du bist hier also nicht alleine. Möchtest du auf die Person warten oder dich verstecken? 1: Warten. 2: Verstecken\n")
             if (Bibliothek=="1"):
                 print("Mit aufgerissenen Augen stellst du fest, dass da ein kopfloser Ritter mit einem Schwert in der Hand auf dich zu kommt. Du versuchst zu entkommen, doch es ist zu spät. Der kopflose Ritter hat dich erwischt... Ende.")
-            if (Bibliothek=="2" and BistDuMutig=="Ja"):
+            elif (Bibliothek=="2" and BistDuMutig=="Ja"):
                 print("In Windeseile versteckst du dich hinter einem Bücherregal. Du spähst hinter deiner Deckung hervor und erkennst schockiert, dass ein kopfloser Ritter mit einem Schwert in der Hand durch die Bibliothek stampft. Mutig wie du bist, gibst du keinen Laut von dir. Du wartest bis der schaurige Ritter verschwunden ist und kommst langsam hinter deinem Versteck hervor.")
-                MutigInDerBibliothek= input("Der kopflose Ritter ist die Treppe hinaufgegangen. Um die Bibliothek nun zu verlassen drücke 1\n")
+                MutigInDerBibliothek=input("Der kopflose Ritter ist die Treppe hinaufgegangen. Um die Bibliothek nun zu verlassen drücke 1\n")
             else:
                 print("In Windeseile versteckst du dich hinter einem Bücherregal. Du spähst hinter deiner Deckung hervor und erkennst schockiert, dass ein kopfloser Ritter mit einem Schwert in der Hand durch die Bibliothek stampft. Vor lauter Panik stößt du das Bücherregal um. Obwohl der Ritter keinen Kopf hat, scheint er dich bemerkt zu haben. Du versuchst vor ihm davon zu laufen, doch er ist unnatürlich schnell. Nach einer kurzen Verfolgungsjagd durch die Bibliothek hat er dich erwischt... Ende.")
-        if (Flur=="2"):
+    if (Flur=="2"):
             print("Du kommst an einem Gemälde vorbei.")
-            Gemälde= ("Möchtest du es näher betrachten? 1: Ja. 2: Nein. ")
+            Gemälde=input("Möchtest du es näher betrachten? 1: Ja. 2: Nein.\n")
             if (Gemälde=="1"):
-                print("Auf dem Gemälde ist eine junge Frau mit langen blonden Haar zu sehen, die eine rote Rose in der Hand hält. Sie lächelt den Betrachter an. Das erfreut den Kunstliebhaber!")
+                print("Auf dem Gemälde ist eine junge Frau mit langen blonden Haar zu sehen, die eine rote Rose in der Hand hält. Sie lächelt den Betrachter an.")
+                IchMagKunst=input("Das erfreut den Kunstliebhaber! Drücke um fortzufahren 1\n")
+            elif (Gemälde=="2"):
+                 print(f"Kunst hat scheint dich nicht sonderlich zu begeistern {DeinName}. Zumindest, wenn es um moderne Kunst geht, kann ich das gut verstehen.")
+                 GemaeldeNichtAngesehen=input("Du entscheidest dich dazu weiter zu gehen. Drücke um fortzufahren 1\n")
+                 IchMagKunst=""
+            else:
+                print("fehlerhafte Eingabe")
+            if (IchMagKunst=="1" or GemaeldeNichtAngesehen=="1"): #Bisher Ungelöstes Problem
+                print ("Du gehst einige Schritte weiter und siehst eine Leiter, die auf den Dachboden zu führen scheint. Ob es eine gute Idee, dort hinauf zu gehen?")
+                DachbodenLeiter=input("Drücke um fortzufahren 1\n")
+            else:
+                print ("fehlerhafte Eingabe")
+            if (DachbodenLeiter=="1" and BistDuMutig=="Ja"):
+                print ("Mutig, wie du bist, überlegst du nicht lange und erklimmst den Dachboden. Dort oben ist es stockdunkel, sodass du dich tasten fortbewegst. Plötzlich fühlst du einen durchdringenden Schmerz in deiner Hand. Du hast in eine Mausefalle gefasst! Autsch!")
+                Mausefalle=input("Du jaulst laut auf und zappelst mit der Hand hin und her. Was tust du denn jetzt bloß? 1: Ganz ruhig bleiben und dich erinnern, was dein Großvater dir in solchen Situationen geraten hat. 2: Dich nach einem Hilfsmittel umsehen\n")
+
+
 
